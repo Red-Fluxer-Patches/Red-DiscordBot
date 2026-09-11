71be72143a78f4b2a738fd0be6c7e2e6073e4960
Jakub Kuczys <me@jacken.men>
2026-09-11 22:18:39 +0200

    Stop asking for owner ID during initial configuration

--- redbot/__main__.py
+++ redbot/__main__.py
@@ -119,7 +119,7 @@ async def _edit_token(red, token, no_prompt):
             return
         await red._config.token.set(token)
     elif not no_prompt and confirm("Would you like to change instance's token?", default=False):
-        await interactive_config(red, False, True, False, print_header=False)
+        await interactive_config(red, False, True, print_header=False)
         print("Token updated.\n")
 
 
@@ -349,14 +349,12 @@ async def run_bot(red: Red, cli_flags: Namespace) -> None:
         if not token:
             token = await red._config.token()
 
-    owner_set = cli_flags.owner or cli_flags.co_owner or await red._config.owner() is not None
-
     prefix = cli_flags.prefix or await red._config.prefix()
 
-    if not (token and prefix and owner_set):
+    if not (token and prefix):
         if cli_flags.no_prompt is False:
             new_token = await interactive_config(
-                red, token_set=bool(token), prefix_set=bool(prefix), owner_set=owner_set
+                red, token_set=bool(token), prefix_set=bool(prefix)
             )
             if new_token:
                 token = new_token
